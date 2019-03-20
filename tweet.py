0b7e373231e2d0a4db2d29a08ef800a479872fbc
Tola's Terminal <sohbaker@gmail.com>
2019-03-20 20:05:46 +0000

    initial files for application

---
+++ tweet.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import tweepy
+
+app = Flask("CFGPy")
+
+with open("credentials.txt", "r") as file:
+    consumer_key = file.readline().split()[2]
+    consumer_secret = file.readline().split()[2]
+    access_token = file.readline().split()[2]
+    access_token_secret = file.readline().split()[2]
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+@app.route("/") 
+def main():
+    return render_template("main.html")
+
+@app.route("/timeline", methods=["GET"]) 
+def timeline():
+    tweets_from_timeline = api.home_timeline()
+    return render_template("main.html", tweets_from_timeline=tweets_from_timeline)
+
+@app.route("/tweet", methods=["POST"]) 
+def tweet():
+    text = request.form['Tweet'] # [name] must match the name= shown on the html form input type
+    post_tweet = api.update_status(text)
+    response = "Your tweet was sent: {}".format(text)
+    return render_template("main.html", response=response)
+
+app.run(debug=True)
